4bc2cca8474446d97c2324173d4fe07942ad026f
David <davidkuntz02@gmail.com>
2024-02-26 13:30:34 -0700

    these marbles man

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
-//initial board
-var boardInit = [0,4,4,4,4,4,4,0,4,4,4,4,4,4]
+window.onload = function(){
+    //initial board
+    var boardInit = [0,4,4,4,4,4,4,0,4,4,4,4,4,4];
 
-//marble object function?
-
-//pit object array
-
-//board object
-var Board = {
-    board : boardInit
-
+    var marbles = []; //idk if i need this or not
+    
+    //marble object functions
+    //take marbles from pit and deposit marbles
+    //if last pit is not empty or is store end turn
+    //if last pit is empty capture adjacent marbles and end turn
+    
+    //pit object array?
+    
+    
+    //board object
+    var Board = {
+        board : boardInit,
+        score : {player1: 0, player2: 0},
+        playerTurn : 1
+    }
 }
